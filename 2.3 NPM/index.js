85d21da904c3ca3d645bcd6f682a5316966aeebc
shahdarshilr <shah.darshilr@northeastern.edu>
2024-06-25 08:45:27 -0400

    files added

---
+++ 2.3 NPM/index.js
@@ -0,0 +1,7 @@
+// var generateName = require('sillyname');
+// var sillyName = generateName();
+
+// console.log(`My name is ${sillyName}`);
+
+import {randomSuperhero} from 'superheroes';
+console.log(randomSuperhero());
